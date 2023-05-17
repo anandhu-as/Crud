c1447bda966b8fef630e75ae47a11701f75f1810
anandhu-as <anandhuas010203@gmail.com>
2023-05-17 08:40:58 +0530

    added delete button

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,37 @@
 import React, { useState } from "react";
 import "./App.css";
+let globalID = 0;
 function App() {
   const [todos, setTodos] = useState([]);
   const [todoValue, setTodovalue] = useState("");
   const addTodo = (e) =>
-    e.keyCode === 13 ? setTodos([...todos, todoValue]) : null;
+    e.keyCode === 13
+      ? setTodos([...todos, { todo: todoValue, id: globalID++ }])
+      : null;
+  const deleteTodo = (deleteId) =>
+    setTodos((oldTodos) => oldTodos.filter((item) => item.id !== deleteId));
   return (
     <>
       <h1 className="title">Todo App</h1>
-      <input className="input"
+      <input
+        className="input"
         type="text"
         value={todoValue}
         onChange={(e) => setTodovalue(e.target.value)}
         onKeyDown={addTodo}
       />
       <ul className="todo-list">
-        {todos.map((todo,index) => {
-          return <li key={index} className="todo-item">{todo}</li>;
+        {todos.map((item, i) => {
+          return (
+            <div key={item.id} className="container">
+              <li className="todo-item">{item.todo}</li>
+              <button onClick={() => deleteTodo(item.id)}> ❌</button>
+            </div>
+          );
         })}
       </ul>
       <footer className="footer">@anandhu-as</footer>
     </>
   );
 }
-
 export default App;
