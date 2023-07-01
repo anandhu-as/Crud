651d4480e574e55b417771bb19a5720ffdcbf6fc
anandhu-as <anandhuas010203@gmail.com>
2023-07-01 14:35:46 +0530

    used useref hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,26 @@
-import React, { useState } from "react";
-
+import React, { useEffect, useRef, useState } from "react";
 const App = () => {
   const [inputvalue, setInputValue] = useState("");
   const [todos, setTodos] = useState([]);
   const handleChange = (event) => setInputValue(event.target.value);
   const handleClick = () => {
     setTodos([...todos, inputvalue]);
-    setInputValue('')
+    setInputValue("");
   };
+  const inputRef = useRef();
+  {
+    /* to focus on inputField */
+  }
 
+  useEffect(() => inputRef.current.focus());
   return (
     <div>
-      <input type="text" value={inputvalue} onChange={handleChange} />
+      <input
+        type="text"
+        value={inputvalue}
+        onChange={handleChange}
+        ref={inputRef}
+      />
       <button onClick={handleClick}>add</button>
       {todos.map((item, index) => {
         return (
@@ -23,5 +32,4 @@ const App = () => {
     </div>
   );
 };
-
 export default App;
