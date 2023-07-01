f1eaf08c6485c790fb698cedcdc113aa8f3e1bb2
anandhu-as <anandhuas010203@gmail.com>
2023-07-01 22:00:35 +0530

    delete todos by id

---
+++ src/Todo.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import { useRef,useState,useEffect } from 'react';
+const Todo = () => {
+const [inputvalue, setInputValue] = useState("");
+  const [todos, setTodos] = useState([]);
+  const handleChange = (event) => setInputValue(event.target.value);
+  const randomID = Math.floor(Math.random() * 100);
+  const handleClick = () => {
+    setTodos([...todos, { todoValue: inputvalue, id: randomID }]);
+    setInputValue("");
+  };
+  const handleDelete = (id) => {
+    setTodos(todos.filter((item) => item.id !== id));
+  };
+  const inputRef = useRef();
+  {
+    /* to focus on inputField */
+  }
+  useEffect(() => inputRef.current.focus());
+  return (
+    <div>
+      <input
+        type="text"
+        value={inputvalue}
+        onChange={handleChange}
+        ref={inputRef}
+      />
+      <button onClick={handleClick}>add</button>
+      {todos.map((item, index) => {
+        return (
+          <div key={index}>
+            <h1>{item.todoValue} </h1>
+            <button onClick={() => handleDelete(item.id)}>❌</button>
+          </div>
+        );
+      })}
+    </div>
+)}
+export default Todo
