ddf67690328c3081d0078d1edb9fa6253b6ed52e
anandhu-as <anandhuas010203@gmail.com>
2023-07-01 22:34:08 +0530

    strike todos

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -1,16 +1,27 @@
-import React from 'react'
-import { useRef,useState,useEffect } from 'react';
+import React from "react";
+import "../src/Styles/Todo.css";
+import { useRef, useState, useEffect } from "react";
 const Todo = () => {
-const [inputvalue, setInputValue] = useState("");
+  const [inputvalue, setInputValue] = useState("");
   const [todos, setTodos] = useState([]);
   const handleChange = (event) => setInputValue(event.target.value);
   const randomID = Math.floor(Math.random() * 100);
   const handleClick = () => {
-    setTodos([...todos, { todoValue: inputvalue, id: randomID }]);
+    setTodos([
+      ...todos,
+      { todoValue: inputvalue, id: randomID, status: false },
+    ]);
     setInputValue("");
   };
   const handleDelete = (id) => {
     setTodos(todos.filter((item) => item.id !== id));
+  };
+  const handleComplete = (id) => {
+    const strike = todos.map((list) => {
+      if (list.id === id) return { ...list, status: true };
+      return list;
+    });
+    setTodos(strike);
   };
   const inputRef = useRef();
   {
@@ -28,12 +39,14 @@ const [inputvalue, setInputValue] = useState("");
       <button onClick={handleClick}>add</button>
       {todos.map((item, index) => {
         return (
-          <div key={index}>
+          <div key={index} className={item.status ? "list" : ""}>
             <h1>{item.todoValue} </h1>
             <button onClick={() => handleDelete(item.id)}>❌</button>
+            <button onClick={() => handleComplete(item.id)}>strike</button>
           </div>
         );
       })}
     </div>
-)}
-export default Todo
+  );
+};
+export default Todo;
