1088c355d2fa438df04345a31c794249621200bf
anandhu-as <anandhuas010203@gmail.com>
2023-10-15 20:33:13 +0530

    xyz

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
-
+import React, { useState } from "react";
 const App = () => {
+  const [todo, setTodo] = useState("");
+  const [todos, setTodos] = useState([]);
+  const addTodo = () =>{
+    setTodos([...todos, todo])
+    setTodos("")
+  };
   return (
-    <div>App</div>
-  )
-}
+    <div>
+      <input type="text" onChange={(event) => setTodo(event.target.value)} />
+      <button onClick={addTodo}>Add</button>
+      {todos.map((data) => {
+        return <h2 key={data}>{data}</h2>;
+      })}
+    </div>
+  );
+};
 
-export default App
+export default App;
